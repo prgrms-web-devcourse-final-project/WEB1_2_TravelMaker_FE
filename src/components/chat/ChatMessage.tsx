c7791aa39dd4c54873bb88717b75da7150b5b599
jeongbaebang <jeongbaebang_dev@naver.com>
2024-11-27 00:14:15 +0900

    feat: 플래너 채팅 메시지 수신, 발신 컴포넌트 (#15)
    - 스토리북 추가

---
+++ src/components/chat/ChatMessage.tsx
@@ -0,0 +1,81 @@
+import EmptyBox from "@common/styles/Empty";
+import { calcResponsiveByPercent } from "@common/styles/theme";
+import { FC } from "react";
+import styled from "styled-components";
+import ChatProfile from "./ChatProfile";
+
+interface SenderMessageProps {
+  text: string;
+}
+
+interface ReceiverMessageProps extends SenderMessageProps {
+  url?: string;
+}
+
+// 상대방 메시지
+const ReceiverMessage: FC<ReceiverMessageProps> = ({ text, url }) => {
+  return (
+    <Container>
+      <ProfileContainer>
+        <ChatProfile size={30} url={url} />
+      </ProfileContainer>
+      <Content $type="receiver">
+        <Label>{text}</Label>
+      </Content>
+      <EmptyBox width={40} height={55} />
+    </Container>
+  );
+};
+
+const ProfileContainer = styled.div`
+  flex-shrink: 0;
+  width: 40px;
+  height: 55px;
+`;
+
+// 본인 메시지
+const SenderMessage: FC<SenderMessageProps> = ({ text }) => {
+  return (
+    <Container>
+      <EmptyBox width={100} height={55} flex={{ grow: 1 }} />
+      <Content $type="sender">
+        <Label>{text}</Label>
+      </Content>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const Content = styled.div<{ $type: "sender" | "receiver" }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: fit-content;
+  max-width: 100%;
+  min-width: 55px;
+  min-height: ${calcResponsiveByPercent(-15, 55)};
+  padding: 17.5px 22px;
+  border-radius: 25px;
+  border-top-right-radius: ${({ $type }) => ($type === "sender" ? "0px" : null)};
+  border-top-left-radius: ${({ $type }) => ($type === "receiver" ? "0px" : null)};
+  background-color: ${({
+    theme: {
+      colors: { primary, secondary },
+    },
+    $type,
+  }) => ($type === "sender" ? primary.strong : secondary.strong)};
+`;
+
+const Label = styled.p`
+  width: 100%;
+  color: ${({ theme }) => theme.colors.background.neutral0};
+  font-size: ${({ theme }) => theme.typography.caption.fontSize};
+  white-space: normal;
+  overflow-wrap: break-word;
+`;
+
+export { ReceiverMessage, SenderMessage };
