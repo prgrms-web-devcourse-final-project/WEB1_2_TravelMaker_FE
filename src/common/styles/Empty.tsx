3689d74ffd47b3db8dbf5a39b7602576e0dd3a8f
jeongbaebang <jeongbaebang_dev@naver.com>
2024-11-27 00:10:27 +0900

    feat: empty box 컴포넌트 flex 옵션 추가 (#15)

--- src/common/styles/Empty.tsx
+++ src/common/styles/Empty.tsx
@@ -4,14 +4,37 @@ import styled from "styled-components";
 interface Props {
   width: number;
   height?: number;
+  flex?: {
+    grow?: number;
+    shrink?: number;
+  };
 }
 
-const EmptyBox: FC<Props> = ({ width, height }) => {
-  return <Container $height={height ?? width} $width={width} />;
+/**
+ * EmptyBox 컴포넌트
+ *
+ * - 레이아웃의 빈 공간을 차지하는 박스 컴포넌트
+ */
+const EmptyBox: FC<Props> = ({ width, height, flex }) => {
+  return (
+    <Container
+      $height={height ?? width}
+      $width={width}
+      $flexGrow={flex?.grow}
+      $flexShrink={flex?.shrink}
+    />
+  );
 };
 
-const Container = styled.div<{ $width: number; $height: number }>`
-  visibility: hidden;
+const Container = styled.div<{
+  $width: number;
+  $height: number;
+  $flexGrow?: number;
+  $flexShrink?: number;
+}>`
+  /* visibility: hidden; */
+  flex-shrink: ${({ $flexShrink }) => $flexShrink ?? 0};
+  flex-grow: ${({ $flexGrow }) => $flexGrow ?? 0};
   width: ${({ $width }) => `${$width}px`};
   height: ${({ $height }) => (typeof $height === "number" ? `${$height}px` : "auto")};
 `;
