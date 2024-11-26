e95d02676ad760b36fe7ad4f80bbaa5195819bec
Young <jny200067@gmail.com>
2024-11-26 09:48:03 +0900

    feat: 일정관리 설명 카드 컴포넌트 구현 (#13)

---
+++ src/components/card/RouteCard.tsx
@@ -0,0 +1,13 @@
+// import styled, {keyframes} from "styled-components";
+// import {FC, useState} from "react";
+
+// import EmptyBox from "@common/styles/Empty";
+// import { calcVwFromPx } from "@common/styles/theme";
+
+// interface RouteCardProps {
+//     onClick: () => void;
+//     title?: string;
+//     country: string;
+//     startDate: string;
+//     endDate: string;
+//   }
