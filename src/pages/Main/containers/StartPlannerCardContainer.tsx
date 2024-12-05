2868c8748731d01d268da4df3122f98388e38df8
jeongbaebang <jeongbaebang_dev@naver.com>
2024-12-06 00:32:17 +0900

    feat: 플래너 시작하기 컴포넌트 비즈니스 로직 추가 (#39)

--- src/pages/Main/containers/StartPlannerCardContainer.tsx
+++ src/pages/Main/containers/StartPlannerCardContainer.tsx
@@ -3,12 +3,21 @@ import StartPlannerCard from "@components/card/StartPlannerCard";
 
 const StartPlannerCardContainer = () => {
   const placeholder = "Room ID";
+
   const navigate = useTypedNavigate();
+
+  const handleCreatePlannerClick = async () => {
+    navigate("/createModalRoom");
+  };
+
+  const handleRoomCodeSubmit = (code: string) => {
+    navigate("/enterModalRoom/:roomId", { roomId: code });
+  };
 
   return (
     <StartPlannerCard
-      onClickPlanner={() => navigate("/createModalRoom")}
-      onSubmit={() => navigate("/enterModalRoom")}
+      onClickPlanner={handleCreatePlannerClick}
+      onSubmit={handleRoomCodeSubmit}
       placeholder={placeholder}
     />
   );
