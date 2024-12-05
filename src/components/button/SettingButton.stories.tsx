6c157627603e5009e7e3ab02fccb135f9e6dc321
Young <jny200067@gmail.com>
2024-12-06 02:00:02 +0900

    feat: 설정 버튼 구성 (#31)

---
+++ src/components/button/SettingButton.stories.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Meta, StoryObj } from "@storybook/react";
+import SettingButton from "@components/button/SettingButton";
+
+const meta: Meta<typeof SettingButton> = {
+  title: "Components/Button/SettingButton",
+  component: SettingButton,
+  argTypes: {},
+};
+
+export default meta;
+
+type Story = StoryObj<typeof SettingButton>;
+
+export const Default: Story = {
+  args: {},
+};
