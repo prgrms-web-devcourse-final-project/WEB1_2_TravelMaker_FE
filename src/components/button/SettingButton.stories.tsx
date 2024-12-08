7da5d11b88ec09015c0e716570fe72df62d5a72f
jeongbaebang <jeongbaebang_dev@naver.com>
2024-12-08 13:16:24 +0900

    refactor: 플래너 설정 버튼 사용자 권한에 따른 조건부 렌더링 추가 (#47)

--- src/components/button/SettingButton.stories.tsx
+++ src/components/button/SettingButton.stories.tsx
@@ -4,7 +4,16 @@ import SettingButton from "@components/button/SettingButton";
 const meta: Meta<typeof SettingButton> = {
   title: "Components/Button/SettingButton",
   component: SettingButton,
-  argTypes: {},
+  decorators: [
+    (Story) => (
+      <div style={{ minWidth: 300, minHeight: 100 }}>
+        <Story />
+      </div>
+    ),
+  ],
+  args: {
+    isHost: true,
+  },
 };
 
 export default meta;
